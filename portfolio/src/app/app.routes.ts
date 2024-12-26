bc3dbf767c9a01a67cb343a23b91dc40dad75041
kklimov1 <kirillvlklimov@gmail.com>
2024-12-26 15:19:31 -0800

    Added routing

--- portfolio/src/app/app.routes.ts
+++ portfolio/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { SparkComponent } from './projects/spark-analysis/spark/spark.component';
+import { StockComponent } from './projects/spark-analysis/stock/stock.component';
+import { AboutMeComponent } from './homepage/about-me/about-me.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "", redirectTo:'/about-me', pathMatch:'full'},
+    {path: 'about-me', component: AboutMeComponent},
+    {path: "sparkAnalysis", component: SparkComponent},
+    {path: "stockAnalysis", component: StockComponent},
+];
+
